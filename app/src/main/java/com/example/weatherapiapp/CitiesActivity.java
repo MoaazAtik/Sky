461dba529b5aa761f989194aec42d2aa1cd6183e
Hakuna <>
2023-12-08 23:08:41 +0300

    storing cities preferences

--- app/src/main/java/com/example/weatherapiapp/CitiesActivity.java
+++ app/src/main/java/com/example/weatherapiapp/CitiesActivity.java
@@ -38,10 +38,8 @@ public class CitiesActivity extends AppCompatActivity {
     private CityListAdapter adapter;
     private List<WeatherReportModelShort> citiesList;
     private AppCompatEditText etCityInput;
-    private Set<String> citiesCountriesNamesSet;
-//    private LinkedHashSet<String> citiesCountriesNamesSet;
-    private LinkedHashSet<String> linkedHashSet;
-//    private Set<String> linkedHashSet;
+//    private Set<String> citiesCountriesNamesSet;
+    private String citiesCountriesNames;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -75,37 +73,88 @@ public class CitiesActivity extends AppCompatActivity {
     // Cities layout methods
     private void prepareData() {
 
-        linkedHashSet = new LinkedHashSet<>();
-
         // get the stored cities preferences set (citiesCountriesNamesSet). Otherwise, initialize a new one.
         // Used LinkedHasSet instead of a HashSet of TreeSet to maintain the cities' order.
-        citiesCountriesNamesSet = getSharedPreferences("MyPrefs", MODE_PRIVATE)
-                .getStringSet(
-                        "citiesCountries",
-                        new LinkedHashSet<>());
-        citiesCountriesNamesSet = new LinkedHashSet<>();
-        Log.d(TAG, "prepareData: Set "+ citiesCountriesNamesSet);
-        Log.d(TAG, "prepareData: list " + citiesList);
+//        citiesCountriesNamesSet = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+//                .getStringSet(
+//                        "citiesCountries",
+//                        new LinkedHashSet<>());
+//        citiesCountriesNamesSet = new LinkedHashSet<>();
+//        Log.d(TAG, "prepareData: Set "+ citiesCountriesNamesSet);
+//        Log.d(TAG, "prepareData: list " + citiesList);
 
-                for (String cityCountryName : citiesCountriesNamesSet) {
-                    getForecastShort(cityCountryName);
+        citiesCountriesNames = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+                .getString(
+                        "citiesCountries",
+                        ""
+                );
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                String currentThreadName = Thread.currentThread().getName();
+                Log.i(TAG, "This thread is: " + currentThreadName);
+
+
+                Log.d(TAG, "run: all cities |" + citiesCountriesNames);
+                String tempCitiesNames = citiesCountriesNames;
+                citiesCountriesNames = ""; // to avoid duplication after calling getForecastShort
+                while (!Objects.equals(tempCitiesNames, "")) {
+                    int i2 = tempCitiesNames.indexOf(" ; ");
+                    Log.d(TAG, "run: i2 " +i2);
+                    String sub = null;
+                    sub = tempCitiesNames.substring(0, i2);
+                    Log.d(TAG, "run: sub |"+sub);
+                    tempCitiesNames = tempCitiesNames.substring(i2 + 3);
+                    Log.d(TAG, "run: tempNames |" +tempCitiesNames);
+                    getForecastShort(sub);
                     Log.d(TAG, "handler");
                     synchronized (this) {
                         try {
-                            wait(80);
+                            // Earlier Calls are not always providing earlier Responses which is Mixing the cities Order. To fix this I let the worker (background) Thread to Wait between API Calls. This gives the calls some time to respond to ensures getting Responses in the order of Calling.
+                            wait(450);
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
                     }
+                    Log.d(TAG, "run: tempCitiesNames.length() " +tempCitiesNames.length());
                 }
 
-//        ExecutorService executor = Executors.newSingleThreadExecutor();
-//        executor.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                String currentThreadName = Thread.currentThread().getName();
-//                Log.i(TAG, "This thread is: " + currentThreadName);
-//
+
+//                String tempCitiesNames = citiesCountriesNames;
+//                citiesCountriesNames = ""; // to avoid duplication after calling getForecastShort
+//                Log.d(TAG, "run: all cities |" + citiesCountriesNames);
+//                while (!Objects.equals(tempCitiesNames, "")) {
+//                    int i2 = tempCitiesNames.indexOf(" ; ");
+//                    Log.d(TAG, "run: i2 " +i2);
+//                    String sub = null;
+//                    sub = tempCitiesNames.substring(0, i2);
+//                    Log.d(TAG, "run: sub |"+sub);
+//                    tempCitiesNames = tempCitiesNames.substring(i2 + 3);
+//                    Log.d(TAG, "run: tempNames |" +tempCitiesNames);
+//                    getForecastShort(sub);
+//                    Log.d(TAG, "run: tempCitiesNames.length() " +tempCitiesNames.length());
+//                }
+
+
+//                String tempCitiesNames = citiesCountriesNames;
+//                Log.d(TAG, "run: all cities ." + citiesCountriesNames);
+//                while (tempCitiesNames.length() > 3) {
+//                    int i1 = tempCitiesNames.indexOf(" ; ") + 3;
+//                    Log.d(TAG, "run: i1 " +i1);
+//                    int i2 = tempCitiesNames.indexOf(" ; ", 3);
+//                    Log.d(TAG, "run: i2 " +i2);
+//                    String sub = null;
+////                    if (i1 != -1) {
+//                        sub = tempCitiesNames.substring(i1, i2);
+//                        Log.d(TAG, "run: sub "+sub);
+//                        tempCitiesNames = tempCitiesNames.substring(i2 + 3);
+//                        Log.d(TAG, "run: tempNames " +tempCitiesNames);
+////                    }
+//                }
+
+
 //                for (String cityCountryName : citiesCountriesNamesSet) {
 //                    getForecastShort(cityCountryName);
 //                    Log.d(TAG, "handler");
@@ -117,11 +166,11 @@ public class CitiesActivity extends AppCompatActivity {
 //                        }
 //                    }
 //                }
-//
-//            }
-//        });
+
+            }
+        });
         Log.i(TAG, "thread " + Thread.currentThread().getName());
-        Log.d(TAG, "prepareData: Set " +citiesCountriesNamesSet);
+//        Log.d(TAG, "prepareData: Set " +citiesCountriesNamesSet);
         Log.d(TAG, "prepareData: list " + citiesList);
 
 //        WeatherReportModelShort weatherReportModelShort = new WeatherReportModelShort();
@@ -156,20 +205,33 @@ public class CitiesActivity extends AppCompatActivity {
                         WeatherReportModelShort weatherReportModelShort = weatherReportModels.get(0);
                         citiesList.add(weatherReportModelShort);
 
-                        citiesCountriesNamesSet.add(weatherReportModelShort.getCity() + " - " + weatherReportModelShort.getCountry());
-                        linkedHashSet.add(weatherReportModelShort.getCity() + " - " + weatherReportModelShort.getCountry());
-//                        Set<String> tempCitySet = new LinkedHashSet<>();
-//                        tempCitySet.addAll(citiesCountriesNamesSet);
-//                        Set<String> tempCitySet = new LinkedHashSet<>(citiesCountriesNamesSet);
-//                        Log.d(TAG, "onResponse: temp set " +tempCitySet);
-                        Log.d(TAG, "onResponse: linkedHashSet " +linkedHashSet);
-                        Log.d(TAG, "onResponse: set "+citiesCountriesNamesSet);
+//                        citiesCountriesNamesSet.add(weatherReportModelShort.getCity() + " - " + weatherReportModelShort.getCountry());
+//                        Log.d(TAG, "onResponse: set "+citiesCountriesNamesSet);
+//                        getSharedPreferences("MyPrefs", MODE_PRIVATE)
+//                                .edit()
+//                                .putStringSet("citiesCountries", citiesCountriesNamesSet)
+//                                .apply();
+//                        Log.d(TAG, "onResponse: set "+citiesCountriesNamesSet);
 
+//                        citiesCountriesNames += " ; "
+//                                + weatherReportModelShort.getCity() + " - " + weatherReportModelShort.getCountry()
+//                                + " ; ";
+
+//                        String currentCity =
+//                                weatherReportModelShort.getCity() + " - " + weatherReportModelShort.getCountry();
+//                        citiesCountriesNames = currentCity // to avoid inverted order at app initialization
+//                                + " ; "
+//                                + citiesCountriesNames;
+
+                        citiesCountriesNames +=
+                                weatherReportModelShort.getCity() + " - " + weatherReportModelShort.getCountry()
+                                + " ; ";
+                        Log.d(TAG, "onResponse: names " + citiesCountriesNames);
                         getSharedPreferences("MyPrefs", MODE_PRIVATE)
                                 .edit()
-                                .putStringSet("citiesCountries", citiesCountriesNamesSet)
+                                .putString("citiesCountries", citiesCountriesNames)
                                 .apply();
-                        Log.d(TAG, "onResponse: set "+citiesCountriesNamesSet);
+                        Log.d(TAG, "onResponse: names " + citiesCountriesNames);
 
                         adapter.notifyDataSetChanged();
                     }
@@ -191,11 +253,11 @@ public class CitiesActivity extends AppCompatActivity {
                     int currentPosition = viewHolder.getAdapterPosition();
                     String cityCountryName = citiesList.get(currentPosition).getCity() + " - " + citiesList.get(currentPosition).getCountry();
                     citiesList.remove(currentPosition);
-                    citiesCountriesNamesSet.remove(cityCountryName);
-                    getSharedPreferences("MyPrefs", MODE_PRIVATE)
-                            .edit()
-                            .putStringSet("citiesCountries", citiesCountriesNamesSet)
-                            .apply();
+//                    citiesCountriesNamesSet.remove(cityCountryName);
+//                    getSharedPreferences("MyPrefs", MODE_PRIVATE)
+//                            .edit()
+//                            .putStringSet("citiesCountries", citiesCountriesNamesSet)
+//                            .apply();
 
                     adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
                     Snackbar snackbar = Snackbar.make(citiesLayout, "City removed", BaseTransientBottomBar.LENGTH_LONG);
